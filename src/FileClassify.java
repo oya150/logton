163b1c0eb0825081a3464f9fa8fe102f9f8bf0c4
tonny.k <tonny.k@kakaocorp.com>
2019-07-05 16:33:08 +0900

    init

--- src/FileClassify.java
+++ src/FileClassify.java
@@ -21,12 +21,16 @@ public class FileClassify extends Classify {
 		List<String> apikeyList = new ArrayList<String>();
 
 //		String[] msgArr = msg.split("]");
-		String regex = "\\[(\\d+)\\]\\[https?:\\/\\/\\w+\\.\\w+\\.\\w+\\/\\w+\\/\\w+\\?(\\w+\\=\\w+\\&)?\\w\\=\\w+\\]\\[(\\w+)\\]\\[[0-9- :]+\\]";
+		String regex = "\\[(\\d+)\\]\\[https?:\\/\\/\\w+\\.\\w+\\.\\w+\\/(\\w+)\\/(\\w+)\\?(\\w+\\=(\\w+)\\&)?\\w\\=(\\w+)\\]\\[(\\w+)\\]\\[[0-9- :]+\\]";
 		Pattern pattern = Pattern.compile(regex);
-		//  \[(\d+)\]\[https?:\/\/\w+\.\w+\.\w+\/\w+\/\w+\?(\w+\=\w+\&)?\w\=\w+\]\[(\w+)\]\[[0-9- :]+\]
+		//  \[(\d+)\]\[https?:\/\/\w+\.\w+\.\w+\/(\w+)\/(\w+)\?(\w+\=(\w+)\&)?\w\=(\w+)\]\[(\w+)\]\[[0-9- :]+\]
 		Matcher matcher = pattern.matcher(msg);
 		while(matcher.find()) {
-			System.out.println("matcher ::" + matcher.group(0));
+			System.out.println("matcher ::" + matcher.group(1));
+			System.out.println("matcher ::" + matcher.group(2));
+			System.out.println("matcher ::" + matcher.group(3));
+			System.out.println("matcher ::" + matcher.group(5));
+			System.out.println("matcher ::" + matcher.group(6));
 		}
 
 		return null;
